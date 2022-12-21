c1e0bccef0d894fce1187c0ef3957a0617edba67
EtoZheLamia <Andariel2175@yandex.ru>
2022-12-21 13:20:22 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//случайное целое положительное число
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+//проверка длины строки
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
